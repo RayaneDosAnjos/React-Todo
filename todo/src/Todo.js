f9093406236d786a25f5bd5fdbec38d4d12dbb7d
rayaneanjos <raydosanjos98@gmail.com>
2023-03-10 21:31:40 -0300

    Desmembrando o formulário

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,38 +1,25 @@
 import React, { useState } from 'react'
 import List from './List'
+import TodoForm from './TodoForm'
 import './Todo.css'
 
 function Todo(){
 
-    const [text, setText] = useState("")
+    
     const [items, setItems] = useState([])
-
-    function handleChange(event){
-        
-        let t = event.target.value
-        setText(t)
+    function onAddItem(item){
+        setItems([...items,item])
     }
-
-    function addItem(event){
-        event.preventDefault()
-        if(text){
-            setItems([...items,text])
-            setText("")
-        }
-    }
-
     return (
         <div className='container'>
         <h1>Todo</h1>
-        <form>
-            <input onChange={handleChange} type='text' value={text}></input>
-            <button onClick={addItem}>Add</button>
-        </form>
-
+        
+        <TodoForm onAddItem={onAddItem}></TodoForm>
         <List items={items}></List>
         </div>
     )
 }
+
 
 
 export default Todo
