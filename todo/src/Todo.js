f66f392931c5a53e794b0c9b61e5c5b65e678422
rayaneanjos <raydosanjos98@gmail.com>
2023-03-13 16:04:36 -0300

    Salvando o estado na memória, e criando um Modal

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import List from './components/List'
 import Item from './components/Item'
 import TodoForm from './components/TodoForm'
 import './Todo.css'
+import Modal from './components/Modal'
+
+const SAVED_ITEMS = "savedItems"
 
 function Todo(){
 
-
     const [items, setItems] = useState([])
-    function onAddItem(text){
-        let it = new Item(text)
 
-        setItems([...items,it])
+    useEffect(()=>{
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS))
+        if(savedItems){
+            setItems(savedItems)
+        }
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items))
+    },[items])
+
+    function onAddItem(text){
+        let item = new Item(text)
+        setItems([...items,item])
     }
+
     function onItemDelete(item){
-        let filteredItems = items.filter(it=>it.id != item.id)
+        let filteredItems = items.filter(it=>it.id !== item.id)
         setItems(filteredItems)
     }
 
     function onDone(item){
         let updatedItems = items.map(it=>{
-            if(it.id == item.id){
+            if(it.id === item.id){
                 it.done = !it.done
             }
             return it
@@ -29,10 +43,12 @@ function Todo(){
     }
     return (
         <div className='container'>
-        <h1>Todo</h1>
+        <header className='header'><h1>Todo</h1><button className='addButton'>+</button></header> 
         
-        <TodoForm onAddItem={onAddItem}></TodoForm>
+        
         <List onDone={onDone} onItemDelete={onItemDelete} items={items}></List>
+
+        <Modal><TodoForm onAddItem={onAddItem}></TodoForm></Modal>
         </div>
     )
 }
---
+++ todo/src/components/Modal.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Card from "./Card"
+
+
+
+function Modal(props){
+    return (
+        <div className="modal">
+            <Card className='cardModal'>
+               {props.children}
+            </Card>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+export default Modal
